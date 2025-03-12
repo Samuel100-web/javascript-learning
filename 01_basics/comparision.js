7623c0cfe1238def47347178ec860faeedbc5fe5
SAMUEL YAQOOB <yaqoobsamuel100@gmail.com>
2025-03-12 13:07:51 +0000

    datatype conversions

---
+++ 01_basics/comparision.js
@@ -0,0 +1,21 @@
+// basic conversion and right and perfect conversions
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//console.log("2" > 1) //2 para hai 1 se, matlab javascrip ne autometicaly 2 do number me convert kar diya hai. ye implicit conversion hai, or Type script ye allow nahi karta hai. iss liy hamesha datatype ka khayal rakhna chahiy or kabi b string ko number se compaire nai karna chahiy
+//console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// in 3 surtoo me value hamesha false hee milti hai. iss tara k conversion ko ovaid karna chahiy
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+

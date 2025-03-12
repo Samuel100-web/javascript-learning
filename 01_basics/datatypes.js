424edecfd129d5cc7f35b06d589d650f93b663cc
SAMUEL YAQOOB <yaqoobsamuel100@gmail.com>
2025-03-12 07:49:08 +0000

    datatypes with notes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+//"use strict" //treat all JS code as newer version
+
+//alert("Hello World"); we are using node.js not browser
+//code readablity is most importent in proramming
+// code readability should be high
+
+let name = "Samuel"
+let age = 33
+let logedIn = true
+
+
+//Premitive Data Types
+
+//symbol => unique,  jab b ham unique k liy bat karty hain to ham symbol prefer karty hain
+//number => 2 power of 53
+//bigint =>
+// boolean => true, false
+//string => ""
+//null => standalone value, this is represent of empty value, null is special value, if we wants to assigne any variable so we can do it. null type is (Object)
+// farz kary k ham ne tempture pata kary k liy request bheji hai or server me kharabi hony ki waja se hamy 0 return huwa hai to iss ka matlab hai k temprature hai. iss liy hamy null value pass kar deny chahiy ta ka kuch b value return no ho saky so null apny app me ek value hai jis ka matlab Empty string.
+//undefined =>
+
+//Objects
+
+console.log(typeof undefined)
